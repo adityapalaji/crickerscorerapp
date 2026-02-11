05fc098fe67e3852b3f80ca3445eb0d811eb3907
Aditya Palaji <adityapalaji@Navyas-MacBook-Pro.local>
2026-02-10 19:21:55 -0600

    added files

---
+++ client/.next/dev/static/development/_buildManifest.js
@@ -0,0 +1,15 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error",
+    "/api",
+    "/api/teams/[teamId]/players",
+    "/not-found",
+    "/scoring-app"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ client/src/pages/api/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import AppRoot from "../src/App"; // change if your root component file is different
+
+export default function Home() {
+  return <AppRoot />;
+}
